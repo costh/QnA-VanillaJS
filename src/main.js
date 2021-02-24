66912e5a9f0653d1bd5784af0054bad15ef64ff1
Hayden Costa <hayden.costa@wundermanthompson.com>
2021-02-24 23:06:09 +0000

    Environment variables

--- src/main.js
+++ src/main.js
@@ -7,19 +7,18 @@ import Answer from './components/Answer/Answer';
 
 const router = new Navigo("/");
 
-
 router.on("/question/:id", async (match) => {
   const id = match?.data?.id;
   if(id){
     await App();
     Answer(id);
   }else{
-    router.navigate('/');
+    router.navigate(`${(process.env.BASE_PUBLIC || '/')}`);
   }
 });
 
 router.on('*',async () => {
-  router.navigate('/');
+  router.navigate(`${process.env.BASE_PUBLIC || '/'}`);
   await App();
   Question();
 })
